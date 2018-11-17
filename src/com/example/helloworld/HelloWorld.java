b134aa6d37f6e7c5c95de6c9b2f3c58967e83c4e
Renáta <renatandreea@gmail.com>
2018-11-17 20:02:33 +0200

    Switch Case

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,7 +1,20 @@
 package com.example.helloworld;
+import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    int number;
+    System.out.println("0. Kilepes\n1. Hello World!\n2. Jatek");
+
+        do {
+            Scanner input = new Scanner(System.in);
+            number = input.nextInt();
+
+            switch (number) {
+                case 1:
+                    System.out.println("Hello, World!");
+                    break;
+            }
+        }while(number!=0);
     }
 }
