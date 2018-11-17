00431f06d75cedbb6ea4bb667aa66715aa1cfa53
Renáta <renatandreea@gmail.com>
2018-11-17 20:51:58 +0200

    Game

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.example.helloworld;
 import java.util.Scanner;
+import java.util.Random;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -8,11 +9,45 @@ public class HelloWorld {
 
         do {
             Scanner input = new Scanner(System.in);
+            System.out.print("\nValassz: ");
             number = input.nextInt();
 
             switch (number) {
                 case 1:
-                    System.out.println("Hello, World!");
+                    System.out.println("\nHello, World!");
+                    break;
+                case 2:
+                    Random rand = new Random();
+                    int  n = rand.nextInt(99) + 1;
+
+                    Scanner input2 = new Scanner(System.in);
+
+                    int lives = 5, m;
+                    boolean win = false;
+
+                    System.out.println("Talald ki milyen szamra gondoltam 1 es 100 kozott! 5 eleted van.");
+
+                    while(lives != 0 && !win){
+                        System.out.print("Tipped: ");
+                        m = input2.nextInt();
+
+                        if (m < n) {
+                            System.out.println("Probalj egy NAGYOBB szamot.");
+                            lives--;
+                        }
+                        else if (m>n) {
+                            System.out.println("Probalj egy KISEBB szamot.");
+                            lives--;
+                        }
+                        else
+                            win = true;
+                        if(lives != 0 && !win)
+                            System.out.println(lives + " eleted maradt");
+                    }
+                    if(win)
+                        System.out.println("NYERTEEL!");
+                    else
+                        System.out.println("Vesztettel...");
                     break;
             }
         }while(number!=0);
